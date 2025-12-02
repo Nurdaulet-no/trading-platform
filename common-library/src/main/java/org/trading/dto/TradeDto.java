6e44d12f7ef30ab24d58487d4901bf73e05ce8d5
Nurdaulet-no <220107075@stu.sdu.edu.kz>
2025-12-02 23:18:29 +0500

    TradeDTO for Spot trade JSON (Binance)

--- common-library/src/main/java/org/trading/dto/TradeDto.java
+++ common-library/src/main/java/org/trading/dto/TradeDto.java
@@ -1,4 +1,26 @@
 package org.trading.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
 public class TradeDto {
+    @JsonProperty("e")
+    private String eventType;
+    @JsonProperty("E")
+    private long eventTime;
+    @JsonProperty("s")
+    private String symbol;
+    @JsonProperty("t")
+    private long tradeId;
+    @JsonProperty("p")
+    private BigDecimal price;
+    @JsonProperty("q")
+    private BigDecimal quantity;
+    @JsonProperty("T")
+    private long tradeTime;
+    @JsonProperty("m")
+    private boolean isBayerMarketMaker;
 }
